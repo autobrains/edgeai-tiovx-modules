23e4a94bf4531db09659fc39216a32d9837d1e34
Rahul T R <r-ravikumar@ti.com>
2023-07-18 13:40:05 +0530

    cmake: common: Update the tidl header path
    Signed-off-by: Rahul T R <r-ravikumar@ti.com>

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -82,7 +82,7 @@ include_directories(${PROJECT_SOURCE_DIR}
                     ${TARGET_FS}/usr/include/processor_sdk/imaging/sensor_drv/include
                     ${TARGET_FS}/usr/include/processor_sdk/imaging/ti_2a_wrapper/include
                     ${TARGET_FS}/usr/include/processor_sdk/imaging/kernels/include
-                    ${TARGET_FS}/usr/include/processor_sdk/tidl_j7/ti_dl/inc
+                    ${TARGET_FS}/usr/include/processor_sdk/tidl_j7/arm-tidl/rt/inc/
                     ${TARGET_FS}/usr/include/processor_sdk/tidl_j7/arm-tidl/tiovx_kernels/include
                     ${TARGET_FS}/usr/include/processor_sdk/tiovx/include
                     ${TARGET_FS}/usr/include/processor_sdk/tiovx/kernels/include
