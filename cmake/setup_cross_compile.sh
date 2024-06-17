239bcc87bbb3b2b9cc82151cc2573c10fa3cebb3
Yarden <yarden.leaf@autobrains.ai>
2024-06-17 16:10:27 +0300

    wip

--- cmake/setup_cross_compile.sh
+++ cmake/setup_cross_compile.sh
@@ -1,4 +1,4 @@
 # Set the following variables as per tool installation on your system
-export CROSS_COMPILER_PATH=/usr/bin
+export CROSS_COMPILER_PATH=/usr/
 export CROSS_COMPILER_PREFIX=aarch64-linux-gnu-
 export TARGET_FS=$HOME/GIT_DIR/REPO/targetfs
